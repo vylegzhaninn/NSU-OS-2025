9d5f89fbb3c1413300a061d87b3c7e957e133dda
Максим Вылегжанин <m.vylegzhanin@g.nsu.ru>
2025-10-09 12:02:52 +0700

    new func OpenFile

--- 24216/m.vylegzhanin/lab3/f.c
+++ 24216/m.vylegzhanin/lab3/f.c
@@ -2,6 +2,16 @@
 #include <unistd.h>
 #include <stdlib.h>
 
+void OpenFile(FILE *fp){
+    fp = fopen("file", "r+");
+    if (fp == NULL) {
+        perror("fopen crashed");
+    } else {
+        printf("File opened successfully.\n");
+        fclose(fp);
+    }
+}
+
 int main(void) {
     FILE *fp;
     uid_t ruid, euid;
@@ -10,13 +20,7 @@ int main(void) {
     euid = geteuid(); // эффективный UID
     printf("Real UID: %d, Effective UID: %d\n", ruid, euid);
 
-    fp = fopen("file", "r+");
-    if (fp == NULL) {
-        perror("first fopen crashed");
-    } else {
-        printf("File opened successfully.\n");
-        fclose(fp);
-    }
+    OpenFile(fp);
 
     if (setuid(ruid) == -1) {
         perror("setuid is crashed");
@@ -27,13 +31,7 @@ int main(void) {
     euid = geteuid(); // эффективный UID
     printf("Real UID: %d, Effective UID: %d\n", ruid, euid);
 
-    fp = fopen("file", "r+");
-    if (fp == NULL) {
-        perror("second fopen crashed");
-    } else {
-        printf("File opened successfully.\n");
-        fclose(fp);
-    }
+    OpenFile(fp);
 
     return 0;
 }
